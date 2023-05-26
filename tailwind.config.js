53e97f8f1e286629661ee22463d1ac6f622f1a87
abhiyadav2345 <abhiyadav2345@gmail.com>
2023-05-26 22:07:24 +0530

    conflicts resolved

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/assets/*/.{png}",
-  ],
+     ],
   theme: {
     extend: {
       backgroundImage: {
