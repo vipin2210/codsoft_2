882c4a4a0d8d7c3e093f4b7f5c343842bb3a9c08
Vipin <2002vipin318@gmail.com>
2023-05-26 22:31:42 +0530

    Added CallToAction Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Subscribe from "@/components/Subscribe";
+import CallToAction from "@/components/CallToAction";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    return (
-        <main>
-            <Subscribe />
-        </main>
-    );
+  return (
+    <main>
+      <CallToAction />
+    </main>
+  );
 }
