b14125166d22b84c93f255bbafea366ea22180e0
abhiyadav2345 <abhiyadav2345@gmail.com>
2023-05-26 21:57:08 +0530

    Navbar Completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Subscribe from "@/components/Subscribe";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ export default function Home() {
     return (
         <main>
             <Subscribe />
+            <Navbar />
         </main>
     );
 }
