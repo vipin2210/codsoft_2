3d55708b31c88ccce1b29dd43b4c8116422cfb5f
harsh <93788928+harsh23079@users.noreply.github.com>
2023-05-27 10:49:31 +0530

    Footer Completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Subscribe1 from "@/components/Subscribe1";
 import Footer from "@/components/Footer";
+import ArtistCard from "@/components/ArtistCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main>
       <Subscribe1 />
+      <ArtistCard/>
       <Footer />
     </main>
   );
