36b4357141b91bfe861125493fea58f1a53b32f7
abhiyadav2345 <abhiyadav2345@gmail.com>
2023-05-27 13:11:24 +0530

    Event Card

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,17 @@ import Subscribe from "@/components/Subscribe";
 import CallToAction from "@/components/CallToAction";
 import Navbar from "@/components/Navbar";
 import Subscribe1 from "@/components/Subscribe1";
+import EventCard from "@/components/EventCard";
 
 const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
     return (
-        <main>
+        <main className="">
+            <Navbar />
             <Subscribe1 />
             <Subscribe />
-            <Navbar />
+            <CallToAction />
+            <EventCard />
         </main>
     );
 }
