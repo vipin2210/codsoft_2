d259d5794aa283dd45de688728149504a4009ab2
abhiyadav2345 <abhiyadav2345@gmail.com>
2023-06-03 10:01:56 +0530

    Footer opening hours changed

--- src/components/CampaignCard.js
+++ src/components/CampaignCard.js
@@ -2,14 +2,16 @@ import React from "react";
 import Carousal from "./Carousal";
 import Link from "next/link";
 
-const CampaignCard = () => {
+const CampaignCard = ({ props }) => {
+    const { name, desc, link, image1, image2, image3 } = props;
+
     return (
         <div>
             <div className=' container mx-auto my-10 '>
                 <div className='grid lg:grid-cols-2 sm:grid-cols-1 justify-center'>
                     <div className='px-4'>
                         <h1 className='text-white font-bold text-4xl sm:text-6xl '>
-                            Singing Competition By Help Artist India
+                            {name}
                         </h1>
                         <div className='text-white  mt-4'>
                             <hr className='  border-2 w-[30%]' />
@@ -17,29 +19,13 @@ const CampaignCard = () => {
 
                             <div className='my-8'>
                                 <p className='my-2'>
-                                    Lorem ipsum dolor sit amet consectetur.
-                                    Ullamcorper viverra tellus malesuada gravida
-                                    nibh consequat in ut. Nibh enim arcu ac
-                                    volutpat sed sit urna enim urna. Pretium
-                                    massa scelerisque nunc quis velit risus
-                                    purus erat. Mi magna fermentum scelerisque
-                                    pellentesque id arcu. Morbi viverra lacus
-                                    aliquam sed. Aenean viverra eget malesuada
-                                    proin sed. Duis leo mauris leo dapibus .
+                                   {desc}
                                 </p>
                                 <p className='my-2'>
-                                    Lorem ipsum dolor sit amet consectetur.
-                                    Ullamcorper viverra tellus malesuada gravida
-                                    nibh consequat in ut. Nibh enim arcu ac
-                                    volutpat sed sit urna enim urna. Pretium
-                                    massa scelerisque nunc quis velit risus
-                                    purus erat. Mi magna fermentum scelerisque
-                                    pellentesque id arcu. Morbi viverra lacus
-                                    aliquam sed. Aenean viverra eget malesuada
-                                    proin sed. Duis leo mauris leo dapibus.
+                                 
                                 </p>
                             </div>
-                            <Link href="/donation">
+                            <Link href='/donation'>
                                 <button className='lg:text-2xl sm:text-xl p-4 px-8  font-bold border-4 rounded-xl border-w  mb-8 hover:border-yellow-300 hover:text-yellow-300 '>
                                     DONATE NOW
                                 </button>
