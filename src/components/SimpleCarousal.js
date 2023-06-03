099de051f896e4d2362bdcde56754a4264a65098
Vipin <2002vipin318@gmail.com>
2023-06-04 00:33:33 +0530

    resolved responsive pastEvent page and simpleCarousal

--- src/components/SimpleCarousal.js
+++ src/components/SimpleCarousal.js
@@ -5,8 +5,8 @@
 // import "swiper/css/pagination";
 // import "swiper/css/navigation";
 // import { Pagination, Navigation } from "swiper";
-// import Glider from "react-glider";
-// import "glider-js/glider.min.css";
+import Glider from "react-glider";
+import "glider-js/glider.min.css";
 
 // const SimpleCarousal = () => {
 //     return (
@@ -80,7 +80,43 @@
 import React from "react";
 
 const SimpleCarousal = () => {
-    return <div>SimpleCarousal</div>;
+  return (
+    <div>
+      {" "}
+      <Glider
+        className="glider-container p-5 "
+        // draggable
+        scrollPropagate
+        hasArrows
+        hasDots
+        // itemWidth={6}
+        slidesToShow={1}
+        rewind
+        // exactWidth
+        responsive={[
+          {
+            breakpoint: 850,
+            settings: {
+              slidesToShow: 2,
+            },
+          },
+          {
+            breakpoint: 1100,
+            settings: {
+              slidesToShow: 2,
+            },
+          },
+        ]}
+      >
+        <div className="h-40">
+          <img className="h-full" src="/serviceImg1.png" />
+        </div>
+        <div className="h-40">
+          <img className="h-full" src="/serviceImg2.png" />
+        </div>
+      </Glider>
+    </div>
+  );
 };
 
 export default SimpleCarousal;
