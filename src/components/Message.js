9b88b94ab5eca8756ead4092a6fdf5440dee030d
unknown <nancykumari620@gmail.com>
2023-06-07 01:16:23 +0530

    Message

---
+++ src/components/Message.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Glider from 'react-glider';
+import 'glider-js/glider.min.css';
+const Message = () => {
+
+  return (
+    <div>
+
+
+      <hr className='w-11/12 h-1.5 mx-auto border-0 rounded bg-[#E2B55E]'></hr>
+
+      <div className='bg-[#0D0F27] w-full h-5/6 p-8'>
+        <h1 className='font-bold text-white text-center text-4xl'>
+          <div>Legends Message For</div>
+          <div>Help Artist India</div></h1>
+        <section>
+          <Glider
+            draggable
+            hasArrows
+            slidesToShow={1}
+            slidesToScroll={1}
+          >
+
+            <div className='flex flex-row justify-center pl-56 mt-16'>
+              <div className='w-60 h-96 bg-[#D9D9D9] rounded-md'>
+              </div>
+              <div className='w-3/4 text-white p-14 pr-28 flex flex-col text-xl'>
+                Hariharan Anantha Subramani, the living legend, is the winner of the
+                national award "Padma Shri" given by the government of India. We are proud and
+                honored, as is our mentor and guide for "Help Artist India". We appreciate his
+                contribution to the organization.
+                <div className='text-white font-bold text-3xl pt-8'>PADMA SHRI HARIHARAN </div>
+                <div className='font-moon-dance text-white'>Legendary Singer </div>
+              </div>
+            </div>
+          </Glider>
+        </section>
+        <hr className='w-11/12 h-1.5 my-9 border-0 rounded bg-[#E2B55E] mx-auto'></hr>
+      </div>
+
+
+
+    </div>
+
+  )
+}
+
+export default Message
